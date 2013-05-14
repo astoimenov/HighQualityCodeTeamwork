100c5d5e3ea4c875fd3dd9edf014425e7245d11c
Sabrie Nedzhip <sabrie.nedzhip@gmail.com>
2013-05-15 00:04:30 +0300

    Added UnitTests for method Equals(); in Vector.cs

---
+++ KingSurvival.Tests/VectorTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KingSurvival;
+
+[TestClass]
+public class VactorTests
+{
+    [TestMethod]
+    public void TestEqualsTwoEqualVectors()
+    {
+        Vector firstVector = new Vector(0, 0);
+        Vector secondVector = new Vector(0, 0);
+
+        bool actual = firstVector.Equals(secondVector);
+        bool expected = true;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void TestEqualsTwoUnequalVectors()
+    {
+        Vector firstVector = new Vector(7, 7);
+        Vector secondVector = new Vector(6, 6);
+
+        bool actual = firstVector.Equals(secondVector);
+        bool expected = false;
+
+        Assert.AreEqual(expected, actual);
+    }
+}
